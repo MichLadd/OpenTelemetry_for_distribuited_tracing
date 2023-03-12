571c0819425e4170c76f0743d569214bc330939b
Michele Laddaga <michele.laddaga@studio.unibo.it>
2023-03-12 23:59:57 +0100

    Final report + docker compose setup

---
+++ src/Constants/TelemetryConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace OTelUseCase
+{    
+    /// <summary>
+     /// The name of the <see cref="ActivitySource"/> that is going to produce our traces.
+     /// </summary>
+    public static class TelemetryConstants
+    {
+        public const string MyAppTraceSource = "OTelUseCase";
+
+        public static readonly Meter DemoMeter = new Meter(MyAppTraceSource);
+
+        public static readonly Counter<long> HitsCounter =
+            DemoMeter.CreateCounter<long>("IndexHits", "hits", "number of hits to homepage");
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -10,10 +10,12 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
+// Creating the logger factory to create the logger used in the application
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
     builder.AddOpenTelemetry(options =>
     {
+        // Creating a console exporter for the LoggerProvider
         options.AddConsoleExporter();
     });
 });
@@ -26,12 +28,19 @@ var resource = ResourceBuilder.CreateDefault().AddService("OTelUseCase");
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Configuration.AddEnvironmentVariables();
 // Add tracing auto instrumentation
 builder.Services.AddOpenTelemetryTracing(b =>
 {
+    Environment.GetEnvironmentVariable("JAEGER_SERVICE_NAME");
     // uses the default Jaeger settings
-    b.AddJaegerExporter();
-
+    //b.AddJaegerExporter();
+    // get Jaeger configuration parameters from environment variables
+    b.AddJaegerExporter(o =>
+     {
+         o.AgentHost = builder.Configuration.GetValue<string>("Jaeger:Host");
+         o.AgentPort = builder.Configuration.GetValue<int>("Jaeger:Port");
+     });
     // receive traces from our own custom sources
     b.AddSource(TelemetryConstants.MyAppTraceSource);
 
@@ -48,8 +57,8 @@ builder.Services.AddOpenTelemetryTracing(b =>
 // Add metrics auto instrumentation
 builder.Services.AddOpenTelemetryMetrics(b =>
 {
-    // add prometheus exporter
-    b.AddPrometheusExporter();
+    // add prometheus exporter if needed
+    //b.AddPrometheusExporter();
 
     // receive metrics from our own custom sources
     b.AddMeter(TelemetryConstants.MyAppTraceSource);
@@ -77,7 +86,6 @@ logger.LogInformation($"Welcome to {Assembly.GetEntryAssembly().GetName().Name}.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
